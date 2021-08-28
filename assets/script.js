c5fce44ee24edde0991a0779ea6ce909efe06cbb
joaomarcos17 <joaomarcoscalacia@hotmail.com>
2021-08-28 17:12:13 +0100

    first commit

---
+++ assets/script.js
@@ -0,0 +1,70 @@
+// navbar content!!
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    // Get all "navbar-burger" elements
+    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+  
+    // Check if there are any navbar burgers
+    if ($navbarBurgers.length > 0) {
+  
+      // Add a click event on each of them
+      $navbarBurgers.forEach( el => {
+        el.addEventListener('click', () => {
+  
+          // Get the target from the "data-target" attribute
+          const target = el.dataset.target;
+          const $target = document.getElementById(target);
+  
+          // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+          el.classList.toggle('is-active');
+          $target.classList.toggle('is-active');
+  
+        });
+      });
+    }
+  
+  });
+
+  // end of navbar 
+
+
+  // modal 
+
+
+  // modal variables 
+
+  var launcher = document.getElementById('launcher'); // modal button 
+
+  var modal = document.getElementById('page-modal'); 
+
+  // modal page  id="page-modal"
+
+  var Close = document.getElementsByClassName('modal-close')[0]; // button that closes the modal  
+
+    // open the modal 
+  launcher.onclick = function(){
+    modal.style.display = 'block';
+  }
+  // close the modal
+  Close.onclick = function(){      
+    modal.style.display = 'none';
+
+    // end of modal 
+
+  }
+  // footer copyright 
+
+
+  
+  
+
+
+
+
+
+
+
+
+
